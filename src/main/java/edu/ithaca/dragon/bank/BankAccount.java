5499603c35069ccc7ead9dafde53ab113227bd54
KandacePC <krichards1@ithaca.edu>
2020-01-27 10:51:46 -0500

    isEmailValid tests pass

--- src/main/java/edu/ithaca/dragon/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dragon/bank/BankAccount.java
@@ -10,47 +10,63 @@ public class BankAccount {
     /**
      * @throws IllegalArgumentException if email is invalid
      */
-    public BankAccount(String email, double startingBalance){
-        if (isEmailValid(email)){
+    public BankAccount(String email, double startingBalance) {
+        if (isEmailValid(email)) {
             this.email = email;
             this.balance = startingBalance;
-        }
-        else {
+        } else {
             throw new IllegalArgumentException("Email address: " + email + " is invalid, cannot create account");
         }
     }
 
-    public double getBalance(){
+    public double getBalance() {
         return balance;
     }
 
-    public String getEmail(){
+    public String getEmail() {
         return email;
     }
 
     /**
-     * @post reduces the balance by amount if amount is non-negative and smaller than balance
      * @throws InsufficientFundsException if amount is greater than balance
-     * @throws IllegalArgumentException if amount is negative
+     * @throws IllegalArgumentException   if amount is negative
+     * @post reduces the balance by amount if amount is non-negative and smaller than balance
      */
-    public void withdraw (double amount)  {
+    public void withdraw(double amount) {
         balance -= amount;
 
     }
 
 
-    public static boolean isEmailValid(String email){
-        if (email.indexOf('@') == -1){
+    public static boolean isEmailValid(String email) {
+        if (email.indexOf('@') == -1)
             return false;
+
+        ArrayList<String> illegals = new ArrayList<>();
+        illegals.add("#");
+        illegals.add("-");
+        illegals.add("..");
+        illegals.add("_");
+
+        String firstHalfEmail=email.substring(0, email.indexOf('@'));
+
+        for (int i = 0; i < illegals.size(); i++) {
+            if(i==0){
+                if(firstHalfEmail.charAt(0)=='.')
+                    return false;
+            }
+            else if(i== illegals.size()-1){
+                if(firstHalfEmail.charAt(firstHalfEmail.length()-1)=='.')
+                    return false;
+            }
+            if (firstHalfEmail.contains(illegals.get(i)))
+                return false;
         }
-        if (!email.endsWith(".com")){
+
+        if (!email.endsWith(".com"))
             return false;
-        }
-        if(email.substring(0,email.indexOf('@')).contains(".")){//should be changed to incorporate more illegal characters
-            return false;
-        }
-        else{
+        else
             return true;
         }
     }
-}
+
